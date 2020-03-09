b8459b4a7eab9966099d99c837cae4e045239916
Olofsson Robin <roblof-8@student.ltu.se>
2020-03-09 19:10:10 +0100

    first commit

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * Lab5.c
+ *
+ * Created: 2020-03-09 18:24:47
+ * Author : Robin
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
